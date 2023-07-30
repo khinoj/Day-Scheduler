885c433988aab4064a5d84126cbdd461ac6424f9
khinoj <kristopher.hinojosa@gmail.com>
2023-07-30 00:21:19 -0400

    starting over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,12 @@
-var currentDayEl = $('#currentDay');
-var saveBtnEl = $('.saveBtn');
-var timeBlockEl = $('.time-block');
-var descriptionEl = $('.description');
+  // set variable for current date and time
+var now = dayjs();
 
-const currentTime = dayjs().format('H');
-console.log(currentTime);
+// set varible to format current date being displayed at top of page
+var currentDate = now.format('dddd, MMMM D, YYYY');
 
-function displayTime() {
-  var timeNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-  currentDayEl.text(timeNow);
-}
+// set variable for the current hour to adjust color coding
+var currentHour = now.format('H');
+$('#currentDay').text(currentDate);
 
-function changePPP() {
-  for (var i = 9; i < 18; i++) {
-    var hourId = $('hour-' + i);
-    console.log(hourId);
-
-    
-
-  }
-};
-
-
-
-
-setInterval(displayTime, 1000);
-saveBtnEl.on('click', function () {
-  console.log('Got here');
-})
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-
-
-// TODO: Add code to display the current date in the header of the page.
+// variable for the days tasks for each hour
+var daysTasks = ["", "", "", "", "", "", "", "", ""];
